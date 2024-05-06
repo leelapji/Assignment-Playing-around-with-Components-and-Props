9d847c0e0bfc59de7cbc4a84b0300d510e1ea247
leelapji <159124359+leelapji@users.noreply.github.com>
2024-05-06 15:53:52 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/Header';
+import Body from './components/Body';
+import GallaryFooter from './components/GallaryFooter';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Body />
+      <GallaryFooter />
+    </div>
+  );
+}
+
+export default App;
